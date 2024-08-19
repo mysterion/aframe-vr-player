c2e833b925bc34646cb06cd85644b9120e524d43
mysterion <shadowproclaim@gmail.com>
2024-08-19 12:03:31 +0530

    removed 190,200 presets

--- main/components/env/EnvManager.js
+++ main/components/env/EnvManager.js
@@ -14,10 +14,7 @@ export const ENVS = {
 export const PRESET = {
     EQ_180_SBS: 'EQ_180_SBS',
     FE_180_SBS: 'FE_180_SBS',
-    FE_190_SBS: 'FE_190_SBS',
-    FE_200_SBS: 'FE_200_SBS',
-    FLAT_2D: 'FLAT_2D',
-    FLAT_3D: 'FLAT_3D'
+    FLAT_2D: 'FLAT_2D'
 }
 
 
@@ -26,16 +23,10 @@ export const videoPresets = [
         "text": "180 SBS EQR", fn: () => { setAttr(El.env, { 'env-manager': `preset: ${PRESET.EQ_180_SBS}` }) }
     },
     {
-        "text": "FLAT 2D", fn: () => { setAttr(El.env, { 'env-manager': `preset: ${PRESET.FLAT_2D}` }) }
-    },
-    {
         "text": "180 SBS FISH", fn: () => { setAttr(El.env, { 'env-manager': `preset: ${PRESET.FE_180_SBS}` }) }
     },
     {
-        "text": "190 SBS FISH", fn: () => { setAttr(El.env, { 'env-manager': `preset: ${PRESET.FE_190_SBS}` }) }
-    },
-    {
-        "text": "200 SBS FISH", fn: () => { setAttr(El.env, { 'env-manager': `preset: ${PRESET.FE_200_SBS}` }) }
+        "text": "FLAT 2D", fn: () => { setAttr(El.env, { 'env-manager': `preset: ${PRESET.FLAT_2D}` }) }
     }
 ]
 
